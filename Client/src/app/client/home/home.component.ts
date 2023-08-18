51c907e52c756c634953e3798ab2dbbd18d6e7c2
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-08-18 17:42:50 +0700

    Add header

--- Client/src/app/client/home/home.component.ts
+++ Client/src/app/client/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  host: {
+    '(document:click)' : 'onClick($event)',
+  }
 })
 export class HomeComponent implements OnInit {
+  menuOpen = false;
 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  // Remove menu items when click outside
+  onClick(event) {
+    if(!this._eref.nativeElement.contains(event.target)) {
+      this.menuOpen = false;
+    }
+  }
 }
