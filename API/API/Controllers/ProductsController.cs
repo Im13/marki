e70d1613134b7f17444a3dd2ff8085494dd96af2
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-08-27 00:56:01 +0700

    Built skeleton for backend

--- API/API/Controllers/ProductsController.cs
+++ API/API/Controllers/ProductsController.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,16 +9,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts() 
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "This will be a list of products";
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "This will be a single product"; 
+            return await _context.Products.FindAsync(id); 
         }
     }
 }
