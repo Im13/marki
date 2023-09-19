73c0562e5d09e4a1808da0fceac5b78da25a1b56
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-09-19 17:00:55 +0700

    Adding validation for api

--- API/API/DTOs/RegisterDTO.cs
+++ API/API/DTOs/RegisterDTO.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDTO
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*\\d).{4,8}$", ErrorMessage = "Password must be between 4 and 8 digits long and include at least one numeric digit.")]
         public string Password { get; set; }
     }
 }
