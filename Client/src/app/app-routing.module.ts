c68f1d12186af9f579a1f24037ca4c1aecc12e01
Im13 <bachnguyen120195@gmail.com>
2023-09-19 23:56:18 +0700

    Build skeleton for login

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'shop', loadChildren: () => import('./home/home.module').then(m => m.ShopModule) },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule) },
   { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule) },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   { path: 'test-error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
