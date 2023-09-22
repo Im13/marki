8a869e3b9be6ec1d6db9752b124c6e75d6205b57
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-09-22 15:46:54 +0700

    Add async validator

--- Client/src/app/account/register/register.component.ts
+++ Client/src/app/account/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { finalize, map } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  errors: string[] | null = null;
+
   constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
   complexPassword = "(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
   
   registerForm = this.fb.group({
     displayname: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
     password: ['', [Validators.required, Validators.pattern(this.complexPassword)]]
   })
 
@@ -25,8 +28,17 @@ export class RegisterComponent implements OnInit {
   onSubmit() {
     console.log(this.registerForm);
     this.accountService.register(this.registerForm.value).subscribe({
-      next: () => this.router.navigateByUrl('/')
+      next: () => this.router.navigateByUrl('/'),
+      error: error => this.errors = error.error
     })
   }
 
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return this.accountService.checkEmailExists(control.value).pipe(
+        map(result => result ? {emailExists: true} : null),
+        finalize(() => control.markAsTouched())
+      )
+    }
+  }
 }
