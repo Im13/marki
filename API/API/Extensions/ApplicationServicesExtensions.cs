f849774ea2988437fdd7441816073a066a841ba9
Im13 <bachnguyen120195@gmail.com>
2023-09-26 09:01:48 +0700

    Uow added

--- API/API/Extensions/ApplicationServicesExtensions.cs
+++ API/API/Extensions/ApplicationServicesExtensions.cs
@@ -25,6 +25,7 @@ namespace API.Extensions
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
