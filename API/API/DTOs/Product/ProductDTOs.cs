301710ca62755698a784e1c24d0a905352d41402
Im13 <bachnguyen120195@gmail.com>
2024-06-17 00:20:43 +0700

    Update product table

--- API/API/DTOs/Product/ProductDTOs.cs
+++ API/API/DTOs/Product/ProductDTOs.cs
@@ -7,6 +7,7 @@ namespace API.DTOs.Product
         public int ProductBrandId { get; set; }
         public int ProductTypeId { get; set; }
         public string ProductSKU { get; set; }
+        public decimal ImportPrice { get; set; }
         public List<ProductOptionDTO> ProductOptions { get; set; }
         public List<ProductSKUDTO> ProductSkus { get; set; }
     }
---
+++ API/Infrastructure/Data/Migrations/20240616155243_UpdateProductDB.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
