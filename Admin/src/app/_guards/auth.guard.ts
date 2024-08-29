6f461769fe4c3e2fcce0927579f00a3fe0ceb429
Im13 <bachnguyen120195@gmail.com>
2024-08-30 01:51:52 +0700

    Add authguard

--- Admin/src/app/_guards/auth.guard.ts
+++ Admin/src/app/_guards/auth.guard.ts
@@ -1,9 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AccountService } from '../_service/account.service';
+import { map } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = (route, state) => {
   // inject() allows you to use dependency injection when you are in an injection context
   const accountService = inject(AccountService);
-  return true;
+  const toastr = inject(ToastrService);
+
+  return accountService.currentUser$.pipe(
+    map(user => {
+        if (user) return true;
+        else {
+            toastr.error('You shall not pass!');
+            return false;
+        }
+    })
+);
+;
 };
--- Admin/src/app/app-routing.module.ts
+++ Admin/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: '', canActivate: , loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'orders', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
-  { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
-  { path: 'customers', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
-  { path: 'statistics', loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule) }
+  { path: '', canActivate: [authGuard], children: [
+    {path: 'home',  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+    { path: 'orders', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
+    { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
+    { path: 'customers', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+    { path: 'statistics', loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule) }
+  ]}
 ];
 
 @NgModule({
