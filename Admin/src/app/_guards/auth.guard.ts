fc4b9e8180963eea5fc4dbdb7db28ffb9df38a86
Im13 <bachnguyen120195@gmail.com>
2024-08-29 23:37:14 +0700

    temp

--- Admin/src/app/_guards/auth.guard.ts
+++ Admin/src/app/_guards/auth.guard.ts
@@ -1,5 +1,9 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../_service/account.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  // inject() allows you to use dependency injection when you are in an injection context
+  const accountService = inject(AccountService);
   return true;
 };
