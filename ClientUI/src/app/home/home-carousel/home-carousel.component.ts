e21def29415fa7d825d809f1164e478f498605b4
Im13 <bachnguyen120195@gmail.com>
2024-09-13 02:21:42 +0700

    Add carousel for home-carousel component

--- ClientUI/src/app/home/home-carousel/home-carousel.component.ts
+++ ClientUI/src/app/home/home-carousel/home-carousel.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import SwiperCore, { Autoplay, SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
+
+SwiperCore.use([Autoplay]);
 
 @Component({
   selector: 'app-home-carousel',
   templateUrl: './home-carousel.component.html',
   styleUrls: ['./home-carousel.component.css']
 })
-export class HomeCarouselComponent {
+export class HomeCarouselComponent implements AfterViewInit {
+  @ViewChild('swiperSlideShow') swiperSlideShow!: SwiperComponent;
+  config: SwiperOptions;
 
+  constructor() {
+    this.config = {
+      slidesPerView: 1,
+      navigation: true,
+      pagination: { clickable: true },
+      scrollbar: { draggable: true },
+      autoplay: {
+        delay: 2000
+      },
+      speed: 300
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.swiperSlideShow.swiperRef.autoplay.start();
+  }
 }
