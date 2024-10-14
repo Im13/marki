d103d7d12e9f0329c05ec17a53f5630002b338f7
Im13 <bachnguyen120195@gmail.com>
2024-10-15 00:32:14 +0700

    Get new arrivals products

--- ClientUI/src/app/new-arrivals/new-arrival-products/new-arrival-products.component.ts
+++ ClientUI/src/app/new-arrivals/new-arrival-products/new-arrival-products.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NewArrivalsService } from '../new-arrivals.service';
+import { Product } from 'src/app/_shared/_models/product';
+import { ProductParams } from 'src/app/_shared/_models/productParams';
 
 @Component({
   selector: 'app-new-arrival-products',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-arrival-products.component.css']
 })
 export class NewArrivalProductsComponent {
+  products: readonly Product[] = [];
+  productParams = new ProductParams();
 
+  constructor(private newArrivalsService: NewArrivalsService){}
+
+  ngOnInit(): void {
+    this.newArrivalsService.getNewArrivals(this.productParams).subscribe({
+      next: response => {
+        //Get first 4 products to display
+        this.products = response.data;
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 }
--- ClientUI/src/app/new-arrivals/new-arrivals.service.ts
+++ ClientUI/src/app/new-arrivals/new-arrivals.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { ProductParams } from '../_shared/_models/productParams';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Pagination } from '../_shared/_models/pagination';
+import { Product } from '../_shared/_models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewArrivalsService {
+  baseApiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNewArrivals(productParams: ProductParams) {
+    let params = new HttpParams();
+
+    params = params.append('search', productParams.search);
+    params = params.append('pageSize', productParams.pageSize);
+    params = params.append('pageIndex', productParams.pageIndex);
+
+    return this.http.get<Pagination<Product[]>>(this.baseApiUrl + 'products', { params });
+  }
 }
