5d6d5b2f605dba3dac88c1e13e0278527b1aa98c
Im13 <bachnguyen120195@gmail.com>
2024-10-18 19:37:51 +0700

    Temp

--- ClientUI/src/app/all-products/all-products.module.ts
+++ ClientUI/src/app/all-products/all-products.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './all-products.component';
 import { CoreModule } from '../_core/core.module';
 import { AllProductsRoutingModule } from './all-products-routing.module';
+import { ProductsFooterComponent } from './products-footer/products-footer.component';
+import { ProductImagesComponent } from './product-images/product-images.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
 
 @NgModule({
   declarations: [
-    AllProductsComponent
+    AllProductsComponent,
+    ProductsFooterComponent,
+    ProductImagesComponent,
+    ProductInfoComponent
   ],
   imports: [
     CommonModule,
