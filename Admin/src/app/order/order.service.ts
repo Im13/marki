cea8d383920cffb4658448dd98861d6e6d274893
Im13 <bachnguyen120195@gmail.com>
2024-12-25 18:49:58 +0700

    Fix upload shopee orders error

--- Admin/src/app/order/order.service.ts
+++ Admin/src/app/order/order.service.ts
@@ -104,7 +104,7 @@ export class OrderService {
       productPropertySKU: orderTableRow[columnDict[OrderConstants.OrderConstants.PRODUCT_PROPERTY_SKU]],
       productSKU: orderTableRow[columnDict[OrderConstants.OrderConstants.SKU]],
       quantity: orderTableRow[columnDict[OrderConstants.OrderConstants.QUANTITY]],
-      returnedQuantity: orderTableRow[columnDict[OrderConstants.OrderConstants.RETURNED_QUANTITY]].toString(),
+      returnedQuantity: orderTableRow[columnDict[OrderConstants.OrderConstants.RETURNED_QUANTITY]]?.toString(),
       salePrice: orderTableRow[columnDict[OrderConstants.OrderConstants.SALE_PRICE]],
       shopDiscount: orderTableRow[columnDict[OrderConstants.OrderConstants.SHOP_DISCOUNT]],
       shopeeSale: orderTableRow[columnDict[OrderConstants.OrderConstants.SHOPEE_SALE]],
