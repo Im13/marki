8622b4b831a7854bb95726653e20144caf2258d9
Im13 <bachnguyen120195@gmail.com>
2025-03-19 18:07:51 +0700

    temp

--- Admin/src/app/website/banner/add-banner-modal/add-banner-modal.component.ts
+++ Admin/src/app/website/banner/add-banner-modal/add-banner-modal.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+
+const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      resolve(reader.result);
+    }
+    reader.onerror = (error) => reject(error);
+  });
 
 @Component({
   selector: 'app-add-banner-modal',
@@ -9,6 +20,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AddBannerModalComponent implements OnInit {
   addBannerFrm: FormGroup;
   isEdit = false;
+  desktopImage: NzUploadFile[] = [];
+  previewImage: string | undefined = '';
+  previewVisible = false;
 
   constructor() {}
 
@@ -20,5 +34,19 @@ export class AddBannerModalComponent implements OnInit {
       altText: new FormControl(''),
       status: new FormControl(true),
     });
+  }
+
+  handlePreview = async (file: NzUploadFile): Promise<void> => {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj!);
+      }
+      this.previewImage = file.url || file.preview;
+      this.previewVisible = true;
+  };
+
+  handleUploadProductChange(info: { file: NzUploadFile }, isMainPhoto: boolean): void {
+    if (info.file.status === 'done') {
+      console.log(info)
+    }
   }
 }
