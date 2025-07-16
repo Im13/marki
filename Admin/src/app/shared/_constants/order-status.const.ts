fa5656a98c83203213c5adbefffd6f99da708f61
Bach Nguyen <bachnguyen120195@gmail.com>
2025-07-16 13:33:04 +0700

    update order statuses in FE

--- Admin/src/app/shared/_constants/order-status.const.ts
+++ Admin/src/app/shared/_constants/order-status.const.ts
@@ -8,4 +8,9 @@ export const ORDER_STATUSES: OrderStatus[] = [
     { id: 5, status: 'Gửi hàng đi' },
     { id: 6, status: 'Huỷ đơn' },
     { id: 7, status: 'Xoá đơn' },
+    { id: 8, status: 'Từ chối nhận hàng' },
+    { id: 9, status: 'Đang hoàn' },
+    { id: 10, status: 'Đã hoàn toàn bộ' },
+    { id: 11, status: 'Hoàn thành' },
+    { id: 12, status: 'Tạo trùng lặp' }
 ];
