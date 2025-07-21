20ff8ad7c548e944c6512bb41c59d3c92b68a923
Bach Nguyen <bachnguyen120195@gmail.com>
2025-07-21 11:19:56 +0700

    Add fb-ads FE

--- Admin/src/app/statistics/meta-ads-dashboard/meta-ads-dashboard.component.ts
+++ Admin/src/app/statistics/meta-ads-dashboard/meta-ads-dashboard.component.ts
@@ -1,12 +1,156 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { endOfMonth, startOfMonth, subDays, subMonths } from 'date-fns';
+
+export interface TreeNodeInterface {
+  key: string;
+  name: string;
+  age?: number;
+  level?: number;
+  expand?: boolean;
+  address?: string;
+  children?: TreeNodeInterface[];
+  parent?: TreeNodeInterface;
+}
 
 @Component({
   selector: 'app-meta-ads-dashboard',
   templateUrl: './meta-ads-dashboard.component.html',
   styleUrls: ['./meta-ads-dashboard.component.css']
 })
-export class MetaAdsDashboardComponent {
+
+export class MetaAdsDashboardComponent implements OnInit {
+  ranges = {
+    'Hôm nay': [new Date(), new Date()],
+    'Hôm qua': [subDays(new Date(), 1), subDays(new Date(), 1)],
+    'Tháng này': [startOfMonth(new Date()), endOfMonth(new Date())],
+    'Tháng trước': [startOfMonth(subMonths(new Date(), 1)), endOfMonth(subMonths(new Date(), 1))]
+  };
+  selectedRange = [new Date(), new Date()];
+
+  ngOnInit(): void {
+    this.listOfMapData.forEach(item => {
+      this.mapOfExpandedData[item.key] = this.convertTreeToList(item);
+    });
+  }
+
+  onChange(dates: Date[]) {
+    // this.dashboardService.getRevenueInRange(dates[0], dates[1]).subscribe({
+    //   next: (response) => {
+    //     this.dateRangeSelected.emit(response);
+    //   },
+    //   error: (error) => {
+    //     console.error('Error fetching revenue in range:', error);
+    //   }
+    // });
+  }
+
   dateRangeSelected() {
     // this.revenueSummary = event;
+  }
+
+  listOfMapData: TreeNodeInterface[] = [
+    {
+      key: `1`,
+      name: 'John Brown sr.',
+      age: 60,
+      address: 'New York No. 1 Lake Park',
+      children: [
+        {
+          key: `1-1`,
+          name: 'John Brown',
+          age: 42,
+          address: 'New York No. 2 Lake Park'
+        },
+        {
+          key: `1-2`,
+          name: 'John Brown jr.',
+          age: 30,
+          address: 'New York No. 3 Lake Park',
+          children: [
+            {
+              key: `1-2-1`,
+              name: 'Jimmy Brown',
+              age: 16,
+              address: 'New York No. 3 Lake Park'
+            }
+          ]
+        },
+        {
+          key: `1-3`,
+          name: 'Jim Green sr.',
+          age: 72,
+          address: 'London No. 1 Lake Park',
+          children: [
+            {
+              key: `1-3-1`,
+              name: 'Jim Green',
+              age: 42,
+              address: 'London No. 2 Lake Park',
+              children: [
+                {
+                  key: `1-3-1-1`,
+                  name: 'Jim Green jr.',
+                  age: 25,
+                  address: 'London No. 3 Lake Park'
+                },
+                {
+                  key: `1-3-1-2`,
+                  name: 'Jimmy Green sr.',
+                  age: 18,
+                  address: 'London No. 4 Lake Park'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      key: `2`,
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+  mapOfExpandedData: { [key: string]: TreeNodeInterface[] } = {};
+
+  collapse(array: TreeNodeInterface[], data: TreeNodeInterface, $event: boolean): void {
+    if (!$event) {
+      if (data.children) {
+        data.children.forEach(d => {
+          const target = array.find(a => a.key === d.key)!;
+          target.expand = false;
+          this.collapse(array, target, false);
+        });
+      } else {
+        return;
+      }
+    }
+  }
+
+  convertTreeToList(root: TreeNodeInterface): TreeNodeInterface[] {
+    const stack: TreeNodeInterface[] = [];
+    const array: TreeNodeInterface[] = [];
+    const hashMap = {};
+    stack.push({ ...root, level: 0, expand: false });
+
+    while (stack.length !== 0) {
+      const node = stack.pop()!;
+      this.visitNode(node, hashMap, array);
+      if (node.children) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push({ ...node.children[i], level: node.level! + 1, expand: false, parent: node });
+        }
+      }
+    }
+
+    return array;
+  }
+
+  visitNode(node: TreeNodeInterface, hashMap: { [key: string]: boolean }, array: TreeNodeInterface[]): void {
+    if (!hashMap[node.key]) {
+      hashMap[node.key] = true;
+      array.push(node);
+    }
   }
 }
