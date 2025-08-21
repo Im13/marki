182f6e54835ab49039817e15d34dcb0604254a27
Bach Nguyen <bachnguyen120195@gmail.com>
2025-08-21 15:22:49 +0700

    responsive header

---
+++ ClientUI/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/index.html',
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
